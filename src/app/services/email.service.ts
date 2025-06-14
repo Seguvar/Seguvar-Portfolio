c9a2bc1831eca6759714cae5491653939056cd84
Cheguvar <seguvar.m@gmail.com>
2025-06-14 18:03:06 +0530

    email config commit

---
+++ src/app/services/email.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface EmailRequest {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+export interface EmailResponse {
+  success: boolean;
+  message: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  private apiUrl = 'http://localhost:8080/api'; // Spring Boot backend URL
+
+  constructor(private http: HttpClient) {}
+
+  sendEmail(emailData: EmailRequest): Observable<EmailResponse> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post<EmailResponse>(`${this.apiUrl}/contact`, emailData, { headers });
+  }
+}
