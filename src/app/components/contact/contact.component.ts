11da9ca261999e42bdb34684e56215436a659476
Cheguvar <seguvar.m@gmail.com>
2025-06-15 10:31:58 +0530

    linkedIn Added commit

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -58,6 +58,16 @@ interface ContactForm {
               <p><a href="https://github.com/Seguvar" target="_blank" rel="noopener">github.com/Seguvar</a></p>
             </div>
             
+            <div class="info-card">
+              <div class="info-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.71-.52-1.248-1.342-1.248-.822 0-1.357.538-1.357 1.248 0 .694.52 1.248 1.327 1.248h.014zm4.908 8.212V9.359c0-.216.016-.432.08-.586.174-.432.57-.88 1.236-.88.872 0 1.22.664 1.22 1.637v3.864h2.4v-4.146c0-2.22-1.184-3.252-2.764-3.252-1.28 0-1.845.7-2.165 1.193v.025h-.014a5.54 5.54 0 0 1 .014-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                </svg>
+              </div>
+              <h3>LinkedIn</h3>
+              <p><a href="https://www.linkedin.com/in/sangara-sequvar-m" target="_blank" rel="noopener">linkedin.com/in/sangara-sequvar-m</a></p>
+            </div>
+            
             <div class="social-links">
               <h3>Connect With Me</h3>
               <div class="social-icons">
@@ -71,6 +81,11 @@ interface ContactForm {
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                     <polyline points="22,6 12,13 2,6"></polyline>
+                  </svg>
+                </a>
+                <a href="https://www.linkedin.com/in/sangara-sequvar-m" target="_blank" rel="noopener" class="social-link">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.71-.52-1.248-1.342-1.248-.822 0-1.357.538-1.357 1.248 0 .694.52 1.248 1.327 1.248h.014zm4.908 8.212V9.359c0-.216.016-.432.08-.586.174-.432.57-.88 1.236-.88.872 0 1.22.664 1.22 1.637v3.864h2.4v-4.146c0-2.22-1.184-3.252-2.764-3.252-1.28 0-1.845.7-2.165 1.193v.025h-.014a5.54 5.54 0 0 1 .014-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
                   </svg>
                 </a>
               </div>
@@ -137,7 +152,7 @@ interface ContactForm {
                   class="btn btn-primary"
                   [disabled]="isSubmitting || !contactForm.valid"
                 >
-                  <span *ngIf="!isSubmitting">Send Message</span>
+                  <span *ngIf="!isSubmitting">Send Message </span>
                   <span *ngIf="isSubmitting" class="loading-text">
                     <div class="loading-spinner"></div>
                     Sending...
